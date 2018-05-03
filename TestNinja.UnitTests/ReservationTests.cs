eb3d119bdbd11b5a69eb72282479c0af6d851f2c
Ian Nastajus <ian.nastajus@gmail.com>
2018-05-03 14:59:01 -0400

    Unit test template format

---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        //3 scenarios
+        // 3 execution paths in that method (3 return possibilities) 
+        public void CanBeCancelledBy_Scenario_ExpectedBehavior()
+        {
+        }
+        public void CanBeCancelledBy_Scenario_ExpectedBehavior2()
+        {
+        }
+        public void CanBeCancelledBy_Scenario_ExpectedBehavior3()
+        {
+        }
+    }
+}
