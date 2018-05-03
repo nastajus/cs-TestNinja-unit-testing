44b2a4d85b8fd62f620062bb388b0d33524d707d
Ian Nastajus <ian.nastajus@gmail.com>
2018-05-03 14:39:38 -0400

    initial commit

---
+++ cs-TestNinja-unit-testing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_TestNinja_unit_testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
