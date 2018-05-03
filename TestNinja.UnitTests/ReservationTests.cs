f95cba4a8e6e91bfc09102c1f8e43ba529b16c98
Ian Nastajus <ian.nastajus@gmail.com>
2018-05-03 15:03:15 -0400

    unit test convention: Arrange, Act, Assert

--- TestNinja.UnitTests/ReservationTests.cs
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -7,9 +7,20 @@ namespace TestNinja.UnitTests
     public class ReservationTests
     {
         [TestMethod]
-        //3 scenarios
+        // 3 scenarios
         // 3 execution paths in that method (3 return possibilities) 
-        public void CanBeCancelledBy_Scenario_ExpectedBehavior()
+        // convention part 1: name of method to test _  part 2: scenario testing _  part 3: expected behavior 
+        public void NameOfMethodToTest_Scenario_ExpectedBehavior()
+        {
+            //convention is called "triple A / AAA" or it's called "Arrange, Act, Assert".
+
+            // Arrange
+
+            // Act
+
+            // Assert 
+        }
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
         {
         }
         public void CanBeCancelledBy_Scenario_ExpectedBehavior2()
